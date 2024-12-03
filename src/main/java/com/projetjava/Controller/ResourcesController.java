bcbef2336658679e83c20c9948d15e4f3cdfa6d6
Tourlat <foucaultfelix33@gmail.com>
2024-12-03 16:48:26 +0100

    Modified ressource

--- src/main/java/com/projetjava/Controller/ResourcesController.java
+++ src/main/java/com/projetjava/Controller/ResourcesController.java
@@ -1,51 +1,3 @@
-// package com.projetjava.Controller;
-
-// import javafx.stage.Stage;
-
-// import com.projetjava.Model.Model;
-// import com.projetjava.View.View;
-// import com.projetjava.View.ViewListener;
-
-// public class Controller implements ViewListener {
-// private final Model model;
-// private final View view;
-// private final BagOfCommands bagOfCommands;
-
-// public Controller(Stage stage) {
-// model = new Model();
-// view = new View(stage);
-// bagOfCommands = new BagOfCommands(model);
-// for (Event event : Event.values()) {
-// view.addButton(event.getText(), event.getId(),
-// geteventHandlerByEvent(event));
-// }
-// model.setListener(view);
-// }
-
-// @Override
-// public void addBuildingButtonClicked() {
-// System.out.println("Le bouton 'Add building' a été cliqué.");
-// bagOfCommands.add(new addBuildingCommand());
-// }
-
-// @Override
-// public void addEmployeesToBuildingButtonClicked() {
-// System.out.println("Le bouton 'Add Employees' a été cliqué.");
-// bagOfCommands.add(new addEmployeesToBuildingCommand());
-// }
-
-// /**
-// */
-// private EventHandler<ActionEvent> geteventHandlerByEvent(Event event) {
-// return switch (event) {
-// case ADD_BUILDING ->
-// _ -> addBuildingButtonClicked();
-// case ADD_EMPLOYEES_TO_BUILDING ->
-// _ -> addEmployeesToBuildingButtonClicked();
-// };
-// }
-
-// }
 package com.projetjava.Controller;
 
 import com.projetjava.Controller.game.GameManager;
@@ -61,6 +13,30 @@ import javafx.scene.image.ImageView;
 public class ResourcesController implements Observer {
 
     @FXML
+    private ImageView foodImage;
+
+    @FXML
+    private ImageView woodImage;
+
+    @FXML
+    private ImageView stoneImage;
+
+    @FXML
+    private ImageView lumberMillImage;
+
+    @FXML
+    private ImageView coalImage;
+
+    @FXML
+    private ImageView ironImage;
+
+    @FXML
+    private ImageView steelImage;
+
+    @FXML
+    private ImageView toolsImage;
+
+    @FXML
     private Label foodLabel;
 
     @FXML
@@ -70,18 +46,22 @@ public class ResourcesController implements Observer {
     private Label stoneLabel;
 
     @FXML
-    private ImageView foodImage;
-
-
-    @FXML
-    private ImageView woodImage;
+    private Label lumberMillLabel;
 
     @FXML
-    private ImageView stoneImage;
+    private Label coalLabel;
+
+    @FXML
+    private Label ironLabel;
+
+    @FXML
+    private Label steelLabel;
+
+    @FXML
+    private Label toolsLabel;
 
     @FXML
     public void initialize() {
-       
         loadImages();
     }
 
@@ -91,57 +71,64 @@ public class ResourcesController implements Observer {
             Image foodImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/Food.png");
             Image woodImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/Wood.png");
             Image stoneImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/Stone.png");
+            Image lumberMillImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/LumberMill.png");
+            Image coalImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/Coal.png");
+            Image ironImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/Iron.png");
+            Image steelImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/Steel.png");
+            Image toolsImg = imageCache.getImage("/com/projetjava/sprites/resources_sprites/Tools.png");
 
-            if (foodImg.isError()) {
-                System.err.println("Error loading food image.");
-            } else {
-                foodImage.setImage(foodImg);
-                foodImage.setFitHeight(100);
-                foodImage.setFitWidth(100);
-                System.out.println("Food image loaded successfully.");
-            }
-
-            if (woodImg.isError()) {
-                System.err.println("Error loading wood image.");
-            } else {
-                woodImage.setImage(woodImg);
-                woodImage.setFitHeight(100);
-                woodImage.setFitWidth(100);
-                System.out.println("Wood image loaded successfully.");
-            }
-
-            if (stoneImg.isError()) {
-                System.err.println("Error loading stone image.");
-            } else {
-
-                stoneImage.setImage(stoneImg);
-                stoneImage.setFitHeight(100);
-                stoneImage.setFitWidth(100);
-                System.out.println("Stone image loaded successfully.");
-            }
+            setImageView(foodImage, foodImg);
+            setImageView(woodImage, woodImg);
+            setImageView(stoneImage, stoneImg);
+            setImageView(lumberMillImage, lumberMillImg);
+            setImageView(coalImage, coalImg);
+            setImageView(ironImage, ironImg);
+            setImageView(steelImage, steelImg);
+            setImageView(toolsImage, toolsImg);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void updateResources(int food, int wood, int stone) {
-        System.out.println(food + " " + wood + " " + stone);
-         Platform.runLater(() -> {
+    private void setImageView(ImageView imageView, Image image) {
+        if (image == null || image.isError()) {
+            System.err.println("Error loading image: " + (image != null ? image.getUrl() : "null"));
+        } else {
+            imageView.setImage(image);
+            imageView.setFitHeight(50);
+            imageView.setFitWidth(50);
+        }
+    }
+
+    public void updateResources(int food, int wood, int stone, int lumberMill, int coal, int iron, int steel, int tools) {
+        System.out.println(food + " " + wood + " " + stone + " " + lumberMill + " " + coal + " " + iron + " " + steel + " " + tools);
+        Platform.runLater(() -> {
             foodLabel.setText("Food: " + food);
             woodLabel.setText("Wood: " + wood);
             stoneLabel.setText("Stone: " + stone);
+            lumberMillLabel.setText("Lumber Mill: " + lumberMill);
+            coalLabel.setText("Coal: " + coal);
+            ironLabel.setText("Iron: " + iron);
+            steelLabel.setText("Steel: " + steel);
+            toolsLabel.setText("Tools: " + tools);
         });
     }
 
     @Override
     public void update() {
         System.out.println("Resources update");
-        
-        int food = GameManager.getInstance().getResourceManager().getResourceQuantity(ResourceType.FOOD);
-        int wood = GameManager.getInstance().getResourceManager().getResourceQuantity(ResourceType.WOOD);
-        int stone = GameManager.getInstance().getResourceManager().getResourceQuantity(ResourceType.STONE);
-        updateResources(food, wood, stone);
+
+        GameManager gameManager = GameManager.getInstance();
+        int food = gameManager.getResourceManager().getResourceQuantity(ResourceType.FOOD);
+        int wood = gameManager.getResourceManager().getResourceQuantity(ResourceType.WOOD);
+        int stone = gameManager.getResourceManager().getResourceQuantity(ResourceType.STONE);
+        int lumberMill = gameManager.getResourceManager().getResourceQuantity(ResourceType.LUMBER);
+        int coal = gameManager.getResourceManager().getResourceQuantity(ResourceType.COAL);
+        int iron = gameManager.getResourceManager().getResourceQuantity(ResourceType.IRON);
+        int steel = gameManager.getResourceManager().getResourceQuantity(ResourceType.STEEL);
+        int tools = gameManager.getResourceManager().getResourceQuantity(ResourceType.TOOL);
+
+        updateResources(food, wood, stone, lumberMill, coal, iron, steel, tools);
     }
 
-    
 }
