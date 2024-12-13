b5ac7a4aec5be994e129b6cb480dde757a4ca037
Guilhem Bonnefous <guilhem.bonnefous@etu.u-bordeaux.fr>
2024-12-14 00:46:03 +0100

    Created diagram using PlantULM

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # projet_approche_objet
 
-run with  : mvn clean javafx:run
+run with : mvn clean javafx:run
+
+To generate diagram : java -DPLANTUML_LIMIT_SIZE=12288 -jar plantuml.jar diagramme.txt
--- src/main/java/com/projetjava/Model/population/Workers.java
+++ src/main/java/com/projetjava/Model/population/Workers.java
@@ -22,7 +22,7 @@ public class Workers implements Population {
 
   /**
    * Get the total number of workers
-   * 
+   *
    * @return Total number of workers
    */
   @Override
@@ -33,7 +33,7 @@ public class Workers implements Population {
 
   /**
    * Get the number of unemployed workers
-   * 
+   *
    * @return Number of unemployed workers
    */
   @Override
@@ -43,7 +43,7 @@ public class Workers implements Population {
 
   /**
    * Get the number of employed workers
-   * 
+   *
    * @return Number of employed workers
    */
   @Override
@@ -53,7 +53,7 @@ public class Workers implements Population {
 
   /**
    * Add employed workers
-   * 
+   *
    * @param employed Number of workers to add
    */
   @Override
@@ -70,12 +70,14 @@ public class Workers implements Population {
 
   /**
    * Add unemployed workers
-   * 
+   *
    * @param unemployed Number of workers to add
    */
   public void addUnemployed(int unemployed) {
     if (unemployed < 0) {
-      throw new IllegalArgumentException("Unemployed workers can't be negative");
+      throw new IllegalArgumentException(
+        "Unemployed workers can't be negative"
+      );
     } else {
       this.unemployedWorkers += unemployed;
     }
@@ -83,13 +85,15 @@ public class Workers implements Population {
 
   /**
    * Remove employed workers
-   * 
+   *
    * @param employed Number of workers to remove
    */
   @Override
   public void removeEmployed(int employed) {
     if (employed < 0) {
-      throw new IllegalArgumentException("Cannot substract by a negative number");
+      throw new IllegalArgumentException(
+        "Cannot substract by a negative number"
+      );
     } else if (employed > this.employedWorkers) {
       throw new IllegalArgumentException("Not enough employed workers");
     } else {
@@ -100,13 +104,15 @@ public class Workers implements Population {
 
   /**
    * Remove unemployed workers
-   * 
+   *
    * @param unemployed Number of workers to remove
    */
   @Override
   public void removeUnemployed(int unemployed) {
     if (unemployed < 0) {
-      throw new IllegalArgumentException("Cannot substract by a negative number");
+      throw new IllegalArgumentException(
+        "Cannot substract by a negative number"
+      );
     } else if (unemployed > this.unemployedWorkers) {
       throw new IllegalArgumentException("Not enough unemployed workers");
     } else {
@@ -116,7 +122,7 @@ public class Workers implements Population {
 
   /**
    * Add workers to the population
-   * 
+   *
    * @param pop Number of workers to add
    */
   @Override
@@ -124,27 +130,9 @@ public class Workers implements Population {
     this.unemployedWorkers += pop;
   }
 
-  // /**
-  // * Remove workers depending on the food available
-  // * @param food Food available
-  // */
-  // @Override
-  // public void foodComsumption(int food) {
-  // if (food < getFoodConsumption()) {
-  // int foodComsumptionForEmployed = getEmployed() * 2;
-  // if (food > foodComsumptionForEmployed) { // Remove unemployed workers first
-  // unemployedWorkers -= food - foodComsumptionForEmployed;
-  // } else {
-  // unemployedWorkers = 0;
-  // employedWorkers -= Math.abs((food - foodComsumptionForEmployed) / 2); // If
-  // it's odd, let a worker live
-  // }
-  // }
-  // }
-
   /**
    * Remove workers depending on the food available
-   * 
+   *
    */
   public void foodConsumption(int foodAvailable) {
     int foodConsumption = getFoodConsumption();
